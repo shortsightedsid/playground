1944b4d97b1ca1d3cfcc5e8f7fa524073ef8acf2
Siddharth Heroor <heroor@ti.com>
2011-06-24 10:15:55 +0530

    scheme.c: More Whitespace cleanup.

--- scheme.c
+++ scheme.c
@@ -858,42 +858,44 @@ static int alloc_cellseg(scheme *sc, int n)
      char *cp;
      long i;
      int k;
-     int adj=ADJ;
+     int adj = ADJ;
      
-     if(adj < sizeof(struct cell)) {
-       adj = sizeof(struct cell);
+     if (adj < sizeof(struct cell)) {
+          adj = sizeof(struct cell);
      }
 
      for (k = 0; k < n; k++) {
-          if (sc->last_cell_seg >= CELL_NSEGMENT - 1)
+          if (sc->last_cell_seg >= CELL_NSEGMENT - 1) {
                return k;
+          }
 
           cp = (char*) sc->malloc(CELL_SEGSIZE * 
                                   sizeof(struct cell) + adj);
 
-          if (cp == 0)
+          if (cp == 0) {
                return k;
+          }
 
           i = ++sc->last_cell_seg ;
           sc->alloc_seg[i] = cp;
 
           /* adjust in TYPE_BITS-bit boundary */
-          if(((unsigned long)cp) % adj != 0) {
+          if (((unsigned long) cp) % adj != 0) {
                cp = (char*) (adj * ((unsigned long) cp / adj + 1));
           }
 
           /* insert new segment in address order */
-          newp=(pointer)cp;
+          newp            = (pointer) cp;
           sc->cell_seg[i] = newp;
 
           while (i > 0 && sc->cell_seg[i - 1] > sc->cell_seg[i]) {
-               p = sc->cell_seg[i];
-               sc->cell_seg[i] = sc->cell_seg[i - 1];
+               p                 = sc->cell_seg[i];
+               sc->cell_seg[i]   = sc->cell_seg[i - 1];
                sc->cell_seg[--i] = p;
           }
 
           sc->fcells += CELL_SEGSIZE;
-          last = newp + CELL_SEGSIZE - 1;
+          last        = newp + CELL_SEGSIZE - 1;
 
           for (p = newp; p <= last; p++) {
                typeflag(p) = 0;
@@ -903,16 +905,17 @@ static int alloc_cellseg(scheme *sc, int n)
 
           /* insert new cells in address order on free list */
           if (sc->free_cell == sc->NIL || p < sc->free_cell) {
-               cdr(last) = sc->free_cell;
+               cdr(last)     = sc->free_cell;
                sc->free_cell = newp;
           } else {
                p = sc->free_cell;
 
-               while (cdr(p) != sc->NIL && newp > cdr(p))
+               while (cdr(p) != sc->NIL && newp > cdr(p)) {
                     p = cdr(p);
+               }
 
                cdr(last) = cdr(p);
-               cdr(p) = newp;
+               cdr(p)    = newp;
           }
      }
 
