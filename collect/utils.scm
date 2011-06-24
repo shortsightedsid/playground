09800985cd4a801c05832ad20fb8cffb7ccf716d
Siddharth Heroor <heroor@ti.com>
2011-06-24 18:24:48 +0530

    Collect/Utils: Add Filter and Some? Utilities

--- collect/utils.scm
+++ collect/utils.scm
@@ -24,3 +24,33 @@
   (if (eq? '() lst) #f
       (let ((checked-results (map predicate? lst)))
         (if (memv #f checked-results) #f #t))))
+
+
+;; Check if some elements of the list passes a predicate.
+;;
+;; @param predicate? - The predicate for each element
+;; @param lst        - The list to check
+;;
+;; @return boolean
+(define (some? predicate? lst)
+  (cond
+   ((null? lst) #f)
+   ((predicate? (car lst)) #t)
+   (else
+    (some? predicate? (cdr lst)))))
+
+
+;; Filter a list against a predicate and returned a list of
+;; filtered elements
+;;
+;; @param predicate? - The predicate for each element
+;; @param lst        - The list to check
+;;
+;; @return list of filtered elements
+(define (filter predicate? lst)
+  (cond
+   ((eq? '() lst) '())
+   ((predicate? (car lst)) 
+    (cons (car lst) (filter predicate? (cdr lst))))
+   (else
+    (filter predicate? (cdr lst)))))
