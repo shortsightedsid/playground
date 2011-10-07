7e3559411d95532cd11fe694e762654222ddfad0
Siddharth Heroor <heroor@ti.com>
2011-10-07 13:39:19 +0530

    src: More minor Whitespace cleanup.

--- src/scheme.c
+++ src/scheme.c
@@ -1832,9 +1832,9 @@ static void finalize_cell(scheme *sc, pointer a)
      if (is_string(a)) {
           sc->free(strvalue(a));
      } else if (is_port(a)) {
-          if (a->_object._port->kind&port_file
-             && a->_object._port->rep.stdio.closeit) {
-               port_close(sc,a,port_input|port_output);
+          if (a->_object._port->kind & port_file && 
+              a->_object._port->rep.stdio.closeit) {
+               port_close(sc, a, port_input | port_output);
           }
 
           sc->free(a->_object._port);
