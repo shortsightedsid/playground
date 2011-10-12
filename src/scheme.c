e7b52ea40bdb3c5ba0434dc8a4ae8db79d53fa02
Siddharth Heroor <heroor@ti.com>
2011-10-12 19:37:58 +0530

    src: Update Makefile and Scheme to use new scheme-num definitions.

--- src/scheme.c
+++ src/scheme.c
@@ -17,6 +17,7 @@
 
 #define _SCHEME_SOURCE
 #include "scheme-private.h"
+#include "scheme-num.h"
 
 #ifndef WIN32
 #include <unistd.h>
@@ -168,19 +169,6 @@ enum scheme_types {
 #define MARK         32768    /* 1000000000000000 */
 #define UNMARK       32767    /* 0111111111111111 */
 
-
-static num num_add(num a, num b);
-static num num_mul(num a, num b);
-static num num_div(num a, num b);
-static num num_intdiv(num a, num b);
-static num num_sub(num a, num b);
-static num num_rem(num a, num b);
-static num num_mod(num a, num b);
-static int num_eq(num a, num b);
-static int num_gt(num a, num b);
-static int num_ge(num a, num b);
-static int num_lt(num a, num b);
-static int num_le(num a, num b);
 
 
 #if USE_MATH
@@ -608,205 +596,6 @@ static void Eval_Cycle(scheme *sc, enum scheme_opcodes op);
 static void assign_syntax(scheme *sc, char *name);
 static int syntaxnum(pointer p);
 static void assign_proc(scheme *sc, enum scheme_opcodes, char *name);
-
-
-#define num_ivalue(n)       (n.is_fixnum?(n).value.ivalue  : (long) (n).value.rvalue)
-#define num_rvalue(n)       (!n.is_fixnum?(n).value.rvalue : (double) (n).value.ivalue)
-
-
-static num num_add(num a, num b)
-{
-     num ret;
-
-     ret.is_fixnum = a.is_fixnum && b.is_fixnum;
-
-     if(ret.is_fixnum) {
-          ret.value.ivalue = a.value.ivalue + b.value.ivalue;
-     } else {
-          ret.value.rvalue = num_rvalue(a) + num_rvalue(b);
-     }
-
-     return ret;
-}
-
-
-static num num_mul(num a, num b) 
-{
-     num ret;
-
-     ret.is_fixnum = a.is_fixnum && b.is_fixnum;
-
-     if(ret.is_fixnum) {
-          ret.value.ivalue = a.value.ivalue * b.value.ivalue;
-     } else {
-          ret.value.rvalue = num_rvalue(a) * num_rvalue(b);
-     }
-
-     return ret;
-}
-
-
-static num num_div(num a, num b)
-{
-     num ret;
-
-     ret.is_fixnum = a.is_fixnum && b.is_fixnum && a.value.ivalue % b.value.ivalue == 0;
-
-     if(ret.is_fixnum) {
-          ret.value.ivalue = a.value.ivalue / b.value.ivalue;
-     } else {
-          ret.value.rvalue = num_rvalue(a) / num_rvalue(b);
-     }
-
-     return ret;
-}
-
-
-static num num_intdiv(num a, num b) 
-{
-     num ret;
-
-     ret.is_fixnum = a.is_fixnum && b.is_fixnum;
-
-     if(ret.is_fixnum) {
-          ret.value.ivalue = a.value.ivalue / b.value.ivalue;
-     } else {
-          ret.value.rvalue = num_rvalue(a) / num_rvalue(b);
-     }
-
-     return ret;
-}
-
-
-static num num_sub(num a, num b) 
-{
-     num ret;
-
-     ret.is_fixnum = a.is_fixnum && b.is_fixnum;
-
-     if(ret.is_fixnum) {
-          ret.value.ivalue = a.value.ivalue - b.value.ivalue;
-     } else {
-          ret.value.rvalue = num_rvalue(a) - num_rvalue(b);
-     }
-
-     return ret;
-}
-
-
-static num num_rem(num a, num b) 
-{
-     num ret;
-     long e1, e2, res;
-
-     ret.is_fixnum = a.is_fixnum && b.is_fixnum;
-
-     e1 = num_ivalue(a);
-     e2 = num_ivalue(b);
-
-     res = e1 % e2;
-
-     /* modulo should have same sign as second operand */
-     if (res > 0) {
-          if (e1 < 0) {
-               res -= labs(e2);
-          }
-     } else if (res < 0) {
-          if (e1 > 0) {
-               res += labs(e2);
-          }
-     }
-
-     ret.value.ivalue=res;
-
-     return ret;
-}
-
-
-static num num_mod(num a, num b) 
-{
-     num ret;
-     long e1, e2, res;
-
-     ret.is_fixnum = a.is_fixnum && b.is_fixnum;
-
-     e1 = num_ivalue(a);
-     e2 = num_ivalue(b);
-
-     res = e1 % e2;
-
-     /* modulo should have same sign as second operand */
-     if (res > 0) {
-          if (e2 < 0) {
-               res *= -1L;
-          }
-     } else {
-          if (e2 > 0) {
-               res *= -1L;
-          }
-     }
-
-     ret.value.ivalue = res;
-
-     return ret;
-}
-
-
-static int num_eq(num a, num b) 
-{
-     int ret;
-     int is_fixnum = a.is_fixnum && b.is_fixnum;
-
-     if(is_fixnum) {
-          ret = a.value.ivalue == b.value.ivalue;
-     } else {
-          ret = num_rvalue(a) == num_rvalue(b);
-     }
-
-     return ret;
-}
-
-
-static int num_gt(num a, num b) 
-{
-     int ret;
-     int is_fixnum = a.is_fixnum && b.is_fixnum;
-
-     if(is_fixnum) {
-          ret = a.value.ivalue > b.value.ivalue;
-     } else {
-          ret = num_rvalue(a) > num_rvalue(b);
-     }
-
-     return ret;
-}
-
-
-static int num_ge(num a, num b)
-{
-     return !num_lt(a,b);
-}
-
-
-static int num_lt(num a, num b) 
-{
-     int ret;
-     int is_fixnum=a.is_fixnum && b.is_fixnum;
-
-     if(is_fixnum) {
-          ret = a.value.ivalue < b.value.ivalue;
-     } else {
-          ret = num_rvalue(a) < num_rvalue(b);
-     }
-
-     return ret;
-}
-
-
-static int num_le(num a, num b) 
-{
-     return !num_gt(a,b);
-}
 
 
 #if USE_MATH
@@ -3982,7 +3771,7 @@ static pointer opexe_2(scheme *sc, enum scheme_opcodes op)
      case OP_ADD:        /* + */
           v = num_zero;
           for (x = sc->args; x != sc->NIL; x = cdr(x)) {
-               v=num_add(v, nvalue(car(x)));
+               v = num_add(v, nvalue(car(x)));
           }
           s_return(sc, mk_number(sc, v));
           
